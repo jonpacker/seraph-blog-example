249d4ffa1c6cbf7591a78113e41d1c9f81bc2a46
Jon Packer <contact@jonpacker.com>
2013-09-30 00:00:20 +0200

    Saving blog posts works

--- app.js
+++ app.js
@@ -6,11 +6,9 @@ var models = require('./models');
 var app = express();
 app.set('view engine', 'jade');
 
-app.use('/api', resource(models.BlogEntry));
+app.use('/api', resource(models.BlogEntry, { strictContentType: false }));
 
 app.get('/', function(req, res, next) {
-    res.render('blog');
-  return;
   models.BlogEntry.findAll(function(err, entries) {
     if (err) return next(err);
     res.render('blog', entries);
